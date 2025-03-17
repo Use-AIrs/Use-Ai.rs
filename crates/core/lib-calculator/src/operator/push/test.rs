f697176986ff99d2d9e3d9970c6c7b29ba387916
Lucca Ramón Ochsner <ochsner.lucca@gmail.com>
2025-03-17 17:47:57 +0100

    fixed tests

--- crates/core/lib-calculator/src/operator/push/test.rs
+++ crates/core/lib-calculator/src/operator/push/test.rs
@@ -2,30 +2,47 @@
 mod tests {
 	use crate::error::Result;
 	use crate::operator::push::*;
-	use crate::MetaData;
 	use cubecl::prelude::*;
+	use cubecl::server::Handle;
 	use cubecl::wgpu::WgpuRuntime;
+	use rand::Rng;
 	use std::time::Instant;
 
-	fn create_meta_vector() -> MetaData {
-		MetaData::build(Box::new([1, 1]), Box::new([1, 6]))
+	fn create_random_vector(
+		client: &ComputeClient<<WgpuRuntime as Runtime>::Server, <WgpuRuntime as Runtime>::Channel>,
+		len: usize,
+		shape: &'static [usize; 2],
+		stride: &'static [usize; 2],
+	) -> TensorHandleRef<'static, WgpuRuntime> {
+		let mut rng = rand::thread_rng();
+		let data: Vec<f32> = (0..len).map(|_| rng.gen()).collect();
+		let handle = client.create(f32::as_bytes(&data));
+		let static_handle: &'static Handle = Box::leak(Box::new(handle));
+		unsafe { TensorHandleRef::from_raw_parts(static_handle, stride, shape, 4) }
 	}
-
-	fn create_meta_matrix() -> MetaData {
-		MetaData::build(Box::new([6, 1]), Box::new([2, 6]))
+	fn create_random_matrix(
+		client: &ComputeClient<<WgpuRuntime as Runtime>::Server, <WgpuRuntime as Runtime>::Channel>,
+		shape: &'static [usize; 2],
+		stride: &'static [usize; 2],
+	) -> TensorHandleRef<'static, WgpuRuntime> {
+		let total = shape[0] * shape[1];
+		let mut rng = rand::thread_rng();
+		let data: Vec<f32> = (0..total).map(|_| rng.gen()).collect();
+		let handle = client.create(f32::as_bytes(&data));
+		let static_handle: &'static Handle = Box::leak(Box::new(handle));
+		unsafe { TensorHandleRef::from_raw_parts(static_handle, stride, shape, 4) }
 	}
 
 	#[test]
 	fn test_prep_square_vector() -> Result<()> {
 		let client = WgpuRuntime::client(&Default::default());
-		let meta = create_meta_vector();
-		let cpu_tensor = meta.cputensor_from_vec::<WgpuRuntime>(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+		let len = 500;
+		let shape = Box::leak(Box::new([1, len]));
+		let stride = Box::leak(Box::new([1, 1]));
+		let dummy_tensor = create_random_vector(&client, len, shape, stride);
 
 		let start = Instant::now();
-		let t: TensorHandleRef<WgpuRuntime> = PrepSquare::push(
-			(&cpu_tensor.meta, &cpu_tensor.handle),
-			&client,
-		)?;
+		let t: TensorHandleRef<WgpuRuntime> = PrepSquare::push((dummy_tensor), &client)?;
 		let output_handle = t.handle.clone();
 
 		let binding = output_handle.binding();
@@ -42,16 +59,13 @@ mod tests {
 	#[test]
 	fn test_prep_square_matrix() -> Result<()> {
 		let client = WgpuRuntime::client(&Default::default());
-		let meta = create_meta_matrix();
-		let cpu_tensor = meta.cputensor_from_vec::<WgpuRuntime>(vec![
-			1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0,
-		]);
+		let len = 500;
+		let shape = Box::leak(Box::new([1, len]));
+		let stride = Box::leak(Box::new([1, 1]));
+		let dummy_tensor = create_random_vector(&client, len, shape, stride);
 		let start = Instant::now();
 
-		let t: TensorHandleRef<WgpuRuntime> = PrepSquare::push(
-			(&cpu_tensor.meta, &cpu_tensor.handle),
-			&client,
-		)?;
+		let t: TensorHandleRef<WgpuRuntime> = PrepSquare::push(dummy_tensor, &client)?;
 		let output_handle = t.handle.clone();
 
 		let binding = output_handle.binding();
@@ -68,21 +82,12 @@ mod tests {
 	#[test]
 	fn test_prep_residuals_vector() -> Result<()> {
 		let client = WgpuRuntime::client(&Default::default());
-		let meta = create_meta_vector();
-		let cpu_tensor = meta.cputensor_from_vec::<WgpuRuntime>(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
-
-		let scalar_handle = client.create(f32::as_bytes(&[3.0]));
+		let shape = Box::leak(Box::new([25, 40]));
+		let stride = Box::leak(Box::new([40, 1]));
+		let dummy_tensor = create_random_matrix(&client, shape, stride);
 		let start = Instant::now();
 
-		let t: TensorHandleRef<WgpuRuntime> = PrepResiduals::push(
-			(
-				&cpu_tensor.meta,
-				&cpu_tensor.handle,
-				&scalar_handle,
-			),
-			&client,
-		)?;
-
+		let t: TensorHandleRef<WgpuRuntime> = PrepSquare::push(dummy_tensor, &client)?;
 		let output_handle = t.handle.clone();
 
 		let binding = output_handle.binding();
@@ -99,24 +104,12 @@ mod tests {
 	#[test]
 	fn test_prep_residuals_matrix() -> Result<()> {
 		let client = WgpuRuntime::client(&Default::default());
-		let meta = create_meta_matrix();
-		let cpu_tensor = meta.cputensor_from_vec::<WgpuRuntime>(vec![
-			1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0,
-		]);
-
-		let scalar_handle = client.create(f32::as_bytes(&[
-			12.0, 11.0, 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0,
-		]));
+		let shape = Box::leak(Box::new([25, 40]));
+		let stride = Box::leak(Box::new([40, 1]));
+		let dummy_tensor = create_random_matrix(&client, shape, stride);
 		let start = Instant::now();
 
-		let t: TensorHandleRef<WgpuRuntime> = PrepResiduals::push(
-			(
-				&cpu_tensor.meta,
-				&cpu_tensor.handle,
-				&scalar_handle,
-			),
-			&client,
-		)?;
+		let t: TensorHandleRef<WgpuRuntime> = PrepSquare::push(dummy_tensor, &client)?;
 		let output_handle = t.handle.clone();
 		let binding = output_handle.binding();
 		let bytes = client.read_one(binding);

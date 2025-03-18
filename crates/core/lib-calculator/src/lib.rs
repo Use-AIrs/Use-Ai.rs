5e42eb018aecd8b55ace262874bcb830a72cb675
Lucca Ramón Ochsner <ochsner.lucca@gmail.com>
2025-03-18 16:59:24 +0100

    cleanup md

--- crates/core/lib-calculator/src/lib.rs
+++ crates/core/lib-calculator/src/lib.rs
@@ -4,25 +4,8 @@ mod model;
 mod operator;
 mod utils;
 
-use cubecl::server::Handle;
+use cubecl::prelude::TensorHandleRef;
 use cubecl::{CubeElement, Runtime};
-
-#[derive(Debug, Clone)]
-pub struct CpuTensor {
-	pub handle: Box<Handle>,
-	pub meta: MetaData,
-}
-
-impl CpuTensor {
-	fn build(
-		handle: Box<Handle>,
-		stride: &[usize],
-		shape: &[usize],
-	) -> Self {
-		let meta = MetaData::build(stride.into(), shape.into());
-		Self { handle, meta }
-	}
-}
 
 #[derive(Debug, Clone)]
 pub struct MetaData {
@@ -37,32 +20,45 @@ impl MetaData {
 	) -> Self {
 		Self { stride, shape }
 	}
+
 	pub fn single() -> Self {
 		Self {
 			stride: Box::new([1, 1]),
 			shape: Box::new([1, 1]),
 		}
 	}
-	pub fn cputensor_empty<R: Runtime>(self) -> CpuTensor {
+
+	pub fn tensorref_empty<'o, R: Runtime>(self) -> TensorHandleRef<'o, R> {
 		let client = R::client(&Default::default());
 		let size_f32 = std::mem::size_of::<f32>();
 		let size = &self.shape.iter().product::<usize>() * size_f32;
-		let handle = client.empty(size);
-		CpuTensor {
-			handle: Box::new(handle),
-			meta: self,
+		let handle = Box::leak(Box::new(client.empty(size)));
+		unsafe {
+			TensorHandleRef::from_raw_parts(
+				handle,
+				Box::leak(self.stride),
+				Box::leak(self.shape),
+				size_f32,
+			)
 		}
 	}
-	pub fn cputensor_from_vec<R: Runtime>(
+
+	pub fn tensorref_from_vec<'o, R: Runtime>(
 		self,
 		input: Vec<f32>,
-	) -> CpuTensor {
-		let client = R::client(&Default::default());
+	) -> TensorHandleRef<'o, R> {
 		let put = input.as_slice();
-		let handle = client.create(f32::as_bytes(put));
-		CpuTensor {
-			handle: Box::new(handle),
-			meta: self,
+		let client = R::client(&Default::default());
+		let handle = Box::leak(Box::new(
+			client.create(f32::as_bytes(put)),
+		));
+		unsafe {
+			TensorHandleRef::from_raw_parts(
+				handle,
+				Box::leak(self.stride),
+				Box::leak(self.shape),
+				4,
+			)
 		}
 	}
 }
@@ -74,14 +70,14 @@ mod test {
 	#[test]
 	fn md_test_empty() {
 		let md = MetaData::build(Box::new([1]), Box::new([1]));
-		let cpu_tensor = MetaData::cputensor_empty::<WgpuRuntime>(md);
+		let cpu_tensor = MetaData::tensorref_empty::<WgpuRuntime>(md);
 		let len = cpu_tensor.handle.size();
 		assert_eq!(len, 4);
 	}
 	#[test]
 	fn md_test_vec() {
 		let md = MetaData::build(Box::new([2, 2]), Box::new([2, 1]));
-		let cpu_tensor = MetaData::cputensor_from_vec::<WgpuRuntime>(md, vec![1.0, 2.0, 3.0, 4.0]);
+		let cpu_tensor = MetaData::tensorref_from_vec::<WgpuRuntime>(md, vec![1.0, 2.0, 3.0, 4.0]);
 		let len = cpu_tensor.handle.size();
 		assert_eq!(len, 16);
 	}
